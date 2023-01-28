5d0468be5390a7447aded51f8c879089e28ab4fb
lizzzshan <odysuessrox@gmail.com>
2023-01-27 16:16:11 -0800

    added an input loop with quit feature, added list of time

--- task.py
+++ task.py
@@ -1,29 +1,23 @@
-import random
-#global variable task array
-tasks = ["Go to gym", "Do an easy Leetcode question", "Do a mediun Leetcode question", "Meditate", "Journal", "Go for a walk"]
+import sys, random
 
+tasks = ["Go to gym", "Do an easy Leetcode question", "Do a medium Leetcode question", "Meditate", "Journal", "Go for a walk"]
+time = ["30 mins", "40 mins", "50 mins", "1 hour", "1 hour 30 mins"]
 def welcomeMessage():
     print("Hello! Welcome to task generator! \n")
     #TODO: add more explanation here
 
-#this function gets user input upon initialization
-#TODO: Turn this into a menu with number input as options
-def getInput():
-    val = input("Enter a number between 1-10: ")
-    number = int(val)
-    if number in range(1,10):
-        print("You have chosen ", number)
-    else:
-        print("Oops! Your option is invalid!")
-    return number
-
 #this function chooses a random task from it's array
 #parameters take in user input, using task array as global variable
-def choosetask(input: int):
-    print(random.choice(tasks))
+def choosetask():
+    while True:
+        print("Your task for the day is to:\n ", random.choice(tasks), " for ", random.choice(time))
+        try_again = input("\nTry again? (Press Enter else n to quit)\n" )
+        if try_again.lower() == "n":
+            break
+    input("\nPress Enter again to exit.")
+
 def main():
     welcomeMessage()
-    num = getInput()
-    choosetask(num)
+    choosetask()
 if __name__ == "__main__":
     main()
